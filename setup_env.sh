7ac19dd54b12c76b0c62e44a0cb46885863428f7
Theodore Patsis <theodore.patsis@gmail.com>
2025-05-21 17:52:06 -0700

    final working version

--- setup_env.sh
+++ setup_env.sh
@@ -2,5 +2,5 @@
 python3 -m venv roberta_env
 source roberta_env/bin/activate
 pip install --upgrade pip
-pip install transformers datasets scikit-learn pandas jupyter
+pip install transformers datasets scikit-learn pandas jupyter 'accelerate>=0.26.0'
 echo "Virtual environment 'roberta_env' set up. Run 'source roberta_env/bin/activate' before launching Jupyter."
